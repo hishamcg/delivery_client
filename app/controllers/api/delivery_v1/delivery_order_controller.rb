63e6fcb6e31598ec57e5b7b8cabbb346e817708b
Hisham <hishamcg@gmail.com>
2015-03-04 15:12:14 +0530

    delivery app changes

--- app/controllers/api/delivery_v1/delivery_order_controller.rb
+++ app/controllers/api/delivery_v1/delivery_order_controller.rb
@@ -3,7 +3,6 @@ class  Api::DeliveryV1::DeliveryOrderController < ApplicationController
 
   def index
     array_out = []
-    p @current_user
     member_order = DeliveryCrossReference.find_todays_orders(@current_user.delivery_person_id, params[:date], params[:slot_id])#.group_by(&:membership_no)
     member_order.collect(&:attributes).each do |goo|
       array_out << goo["delivery_person"].attributes
@@ -33,9 +32,18 @@ class  Api::DeliveryV1::DeliveryOrderController < ApplicationController
   def issue
     member_order = DeliveryCrossReference.issue(params[:membership_no],params[:book_number],
                     "delivery_app_#{@current_user.name}",params[:branch_id],params[:delivery_order_id]).attributes
+    if member_order["errors"].present?
+      success = false
+      errors = member_order["errors"].attributes
+      book_number = member_order["book_number"]
+    else
+      success = true
+      errors = {}
+      book_number = 'B'.to_s + member_order['book_id'].to_s.rjust(6, '0')
+    end
     render :status => 200,
-           :json => { :success => member_order["success"],
-                      :book_number => member_order["book_number"],
-                      :errors => member_order["errors"].attributes }
+           :json => { :success => success,
+                      :book_number => book_number,
+                      :errors => errors}
   end
 end
